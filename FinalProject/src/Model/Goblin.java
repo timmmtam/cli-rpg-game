ef497f83c22214f728e936d0bebdbd7225c7e5a6
Andybunnny <163320980+Andybunnny@users.noreply.github.com>
2026-08-09 10:00:47 +0800

    feat(goblin, dragon): added abstract method for attack target

--- FinalProject/src/Model/Goblin.java
+++ FinalProject/src/Model/Goblin.java
@@ -6,4 +6,9 @@ public class Goblin extends Enemy {
         super(name, health, attackPower, lootChance);
     }
 
+    @Override
+    public void attack(Character target) {
+        System.out.println(getName() + " wildly slashes at " + target.getName() + "!");
+        target.takeDamage(attackPower); // Uses the protected attackPower directly
+    }
 }
