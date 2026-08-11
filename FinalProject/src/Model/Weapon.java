fa4945f1204afb66fcab910c03ea644ebfddc465
timmmtam <tys.timm@gmail.com>
2026-08-11 13:45:26 +0800

    feat: add weapon descriptions in weapon class and allow enemies to drop weapons

--- FinalProject/src/Model/Weapon.java
+++ FinalProject/src/Model/Weapon.java
@@ -6,7 +6,35 @@ public class Weapon extends Item {
 	private int attackDamage;
 	private boolean equipped;
 	
-	public Weapon(String name, String description, int value, int attackDamage) {
+    private static final String[] WEAPON_NAMES = {
+        "Rusty Sword", "Iron Dagger", "Steel Axe", "Battle Hammer",
+        "War Blade", "Spiked Club", "Sharpened Blade", "Cursed Mace",
+        "Dragon Slayer", "Goblin Cleaver"
+    };
+
+    private static final String[] WEAPON_DESCRIPTIONS = {
+        "A dented but serviceable blade.",
+        "A quick and lightweight weapon.",
+        "A heavy axe for devastating strikes.",
+        "Crushing power in every swing.",
+        "A finely crafted sword of war.",
+        "A crude but effective spiked weapon.",
+        "Tempered steel with a keen edge.",
+        "An ominous weapon that hums with dark energy.",
+        "A legendary blade said to pierce dragon scales.",
+        "A massive axe forged from goblin steel."
+    };
+
+    private static final java.util.Random RANDOM = new java.util.Random();
+
+    public static Weapon createRandom() {
+        int idx = RANDOM.nextInt(WEAPON_NAMES.length);
+        int damage = 3 + RANDOM.nextInt(6);
+        int value = 10 + damage * 5;
+        return new Weapon(WEAPON_NAMES[idx], WEAPON_DESCRIPTIONS[idx], value, damage);
+    }
+
+    public Weapon(String name, String description, int value, int attackDamage) {
         super(name, description, value);
         this.attackDamage = attackDamage;
         this.equipped = false;
